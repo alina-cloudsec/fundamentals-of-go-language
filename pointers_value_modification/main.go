ed5095919efd43bbc8ec881fb3be7d9cad42d77c
Alina Moin <alinamoin217@gmail.com>
2026-08-04 10:57:33 -0700

    Add example of pointer value modification in Go
    it is a basic program that demonstrates how to pass a variable's memory address to a function to directly modify its original value from outside the function scope.

---
+++ pointers_value_modification/main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+var x int
+var p *int
+
+func change() 
+{
+	*p = 100                                  // 3. Go inside the address and change value to 100
+}
+
+func main() 
+{
+	x = 20
+	fmt.Println("Before:", x)
+   p = &x                                        //p stores this memory address of x
+	change()                                     // Call function to change the value
+	fmt.Println("After:", x)
+}
