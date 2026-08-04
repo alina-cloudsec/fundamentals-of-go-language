425a4bc87817cbb904583265e79fe621c26a4819
Alina Moin <alinamoin217@gmail.com>
2026-08-04 07:15:25 -0700

    Implement switch case with fallthrough for rewards
    Go's switch cases break automatically without a break keyword. Write a program that forces the code to execute the next immediate case using Go's unique fallthrough keyword.

---
+++ switch_fallthrough/main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main() 
+{
+	userTier:="Gold"
+	fmt.Println("Checking unlocked rewards for your account:")
+
+	switch userTier 
+ {
+	case "Platinum":
+		fmt.Println("Unlocked the Premium Lounge Access")
+		fallthrough                                               // Forces the code to run the next case as well
+	case "Gold":
+		fmt.Println("Unlocked the 20% Discount Code")
+		fallthrough 
+	case "Silver":
+		fmt.Println("Unlocked the Standard Customer Support")
+	default:
+		fmt.Println("Unlocked: Basic Profile Features")
+	}
+}
