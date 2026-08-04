bec55304c6a810d5cafb17e7a2f162cd88954a53
Alina Moin <alinamoin217@gmail.com>
2026-08-04 07:09:48 -0700

    In Go, switch statements don't need a variable to evaluate
    In Go, switch statements don't need a variable to evaluate. it is a conditional program where the switch statement acts as a clean alternative to an if-else-if chain

---
+++ switch_with_no_condition/main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main() 
+{
+	score := 85
+	switch 
+  {
+	case score >= 90:
+		fmt.Println("A+ grade, Excellent performance")
+	case score >= 80:
+		fmt.Println("A grade, Very good")
+	case score >= 70:
+		fmt.Println("B grade, Good job")
+	case score >= 60:
+		fmt.Println("C grade, Needs improvement")
+	default:
+		fmt.Println("F grade, Failed")
+	}
+}
