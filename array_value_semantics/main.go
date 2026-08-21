1b02c48014feb005f4f3d1a5795af8f310612f15
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:33:28 +0500

    Clean up formatting in main function

--- array_value_semantics/main.go
+++ array_value_semantics/main.go
@@ -1,12 +1,11 @@
 package main
 import "fmt"
-func main() 
-{
-	originalScores := [3]int{90, 85, 95}                       //array
+func main() {
+	originalScores := [3]int{90, 85, 95}                            //array
 	copiedScores := originalScores
 	copiedScores[0] = 100
 
 	fmt.Println("--- Array Copy Test ---")
-	fmt.Printf("Original Array: %v\n", originalScores)               //outputOriginal Array: [90 85 95]
-	fmt.Printf("Copied Array:   %v\n", copiedScores)                  //Copied Array:   [100 85 95]
+	fmt.Printf("Original Array: %v\n", originalScores)             //outputOriginal Array: [90 85 95]
+	fmt.Printf("Copied Array:   %v\n", copiedScores)               //Copied Array:   [100 85 95]
 }
