c11feb131469092217bfe4b6076eeca0861a4875
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:55:53 +0500

    Refactor main function and total function formatting

--- variadic_functions/main.go
+++ variadic_functions/main.go
@@ -1,36 +1,30 @@
 package main
 import "fmt"
-
-func total(num ...int) int                          // Variadic function to add 
-{
-	sum:=0
-	for _, val:=range num
-  {
-		sum=sum+val
+func total(num ...int) int {                         // Variadic function to add
+	sum := 0
+	for _, val := range num {
+		sum = sum + val
 	}
 	return sum
 }
 
-func main() 
-{
+func main() {
 	var list []int
 	var x int
 
 	fmt.Println("\t\t\t Simple Go Calculator \t\t\t")
 	fmt.Println("Type a number one by one and press Enter. To stop, just press ENTER:")
 
-	for 
-  {
+	for {
 		fmt.Print("Enter number: ")
-		
-		count, _ :=fmt.Scanln(&x)              //if user presses empty enter, count zero (here & memory operator) count is a counter
-		if count == 0 
-    {
+
+		count, _ := fmt.Scanln(&x)                     //if user presses empty enter, count zero (here & memory operator) count is a counter
+		if count == 0 {
 			break
 		}
-		list = append(list, x)                           //to add anew no in the list (x ko list me add)
+		list = append(list, x)                          //to add anew no in the list (x ko list me add)
 	}
-  
-  ans:=total(list...)
-	fmt.Printf("Total Sum is: %d\n",ans)
+
+	ans := total(list...)
+	fmt.Printf("Total Sum is: %d\n", ans)
 }
