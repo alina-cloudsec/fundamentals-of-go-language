12c3a42c399719159be744d555efdfaa28b80193
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:39:59 +0500

    Simplify function syntax and formatting

--- pointers_value_modification/main.go
+++ pointers_value_modification/main.go
@@ -3,16 +3,14 @@ import "fmt"
 var x int
 var p *int
 
-func change() 
-{
-	*p = 100                                  // 3. Go inside the address and change value to 100
+func change() {
+	*p = 100                             // 3. Go inside the address and change value to 100
 }
 
-func main() 
-{
+func main() {
 	x = 20
 	fmt.Println("Before:", x)
-   p = &x                                        //p stores this memory address of x
-	change()                                     // Call function to change the value
+	p = &x                             //p stores this memory address of x
+	change()                           // Call function to change the value
 	fmt.Println("After:", x)
 }
