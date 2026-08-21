935b980bdc62699f16b586a38128be5cce2f8803
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:43:15 +0500

    Fix print statement and formatting in main function

--- slice_range_loop/main.go
+++ slice_range_loop/main.go
@@ -1,12 +1,10 @@
 package main
 import "fmt"
-func main() 
-{
-	statusCodes := []int{200, 404, 500, 403}                            //dynamic slice
-  
-	for index, value := range statusCodes                       // Using 'range' to extract both index and value cleanly
-  {
-		fmt.Printfln("Index Position: %d",index)
-    fmt.Printfln("Status Code: %d",value)
+func main() {
+	statusCodes := []int{200, 404, 500, 403}
+	
+	for index, value := range statusCodes {
+		fmt.Println("Index Position:", index)
+		fmt.Println("Status Code:", value)
 	}
 }
