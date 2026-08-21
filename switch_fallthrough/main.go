836af59ce688522c2e857063365420109c13c236
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:51:24 +0500

    Simplify switch statement formatting in main.go
    Refactor switch statement for improved readability and consistency.

--- switch_fallthrough/main.go
+++ switch_fallthrough/main.go
@@ -1,18 +1,16 @@
 package main
 import "fmt"
-func main() 
-{
-	userTier:="Gold"
+func main() {
+	userTier := "Gold"
 	fmt.Println("Checking unlocked rewards for your account:")
 
-	switch userTier 
- {
+	switch userTier {
 	case "Platinum":
 		fmt.Println("Unlocked the Premium Lounge Access")
-		fallthrough                                               // Forces the code to run the next case as well
+		fallthrough                                          // Forces the code to run the next case as well
 	case "Gold":
 		fmt.Println("Unlocked the 20% Discount Code")
-		fallthrough 
+		fallthrough
 	case "Silver":
 		fmt.Println("Unlocked the Standard Customer Support")
 	default:
