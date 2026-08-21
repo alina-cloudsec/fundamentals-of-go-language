20b25bd308fe0197c3c2e58f958cf34afac9967a
Alina Moin <alinamoin217@gmail.com>
2026-08-21 20:41:37 +0500

    Improve output formatting in main function
    Updated print statements for better formatting and clarity.

--- short_variable_declarations/main.go
+++ short_variable_declarations/main.go
@@ -1,14 +1,13 @@
 package main
 import "fmt"
 var currentenv = "Development"
-func main() 
-{
-	userName:="Alina"
-	userID:=9091
-	valid:=true
+func main() {
+	userName := "Alina"
+	userID := 9091
+	valid := true
 
 	fmt.Println("\t\t\tUSER PROFILE STATUS")
-	fmt.Printf("User: %s\n",userName)
-  fmt.Printf("ID: %d\n",userID)
-	fmt.Printfln("Verification: %t, Enviroment: %s\n",valid,currentenv)
+	fmt.Println("User:", userName)
+	fmt.Println("ID:", userID)
+	fmt.Println("Verification:", valid, ", Environment:", currentenv)
 }
